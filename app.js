576aaf687d66792b5cd170ac090ce2c5157c3f57
Mustafa Ratlamwala <mustafaratlam110@gmail.com>
2021-12-08 02:46:54 +0500

    Chapter 1 task 5 done

--- app.js
+++ app.js
@@ -1,12 +1,193 @@
-var city = ["karachi", "hyderabad", "quetta", "peshawar"];
+// Chapter 1 Task 1 started
 
-var enterCity = prompt("Enter city name");
+// alert ("Hello World")
 
-enterCity = enterCity.toLocaleLowerCase();
+// Chapter 1 Task 1 ended
+// Chapter 1 Task 2 started
 
-for (var i = 0; i <= city.length; i++){
-    if(enterCity == city[i]){
-        alert("city found")
-    }
+// alert(" Error! Please enter a valid password. ");
 
-};
+// Chapter 1 Task 2 ended
+// Chapter 1 Task 3 started
+
+// alert('Welcome to JS Land... \nHappy Coding!')
+
+// Chapter 1 Task 3 ended
+// Chapter 1 Task 4 started
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! ");
+
+// Chapter 1 Task 4 ended
+// Chapter 1 Task 5 started
+
+// console.log(alert("Hello.. I can run JS through my web browser's console"))
+
+// Chapter 1 Task 5 ended
+// Chapter 1 Task 6 Started
+
+
+
+
+
+// Chapter 1 Task 6 ended
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var days = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
+// var months = [
+//   "Jan",
+//   "Feb",
+//   "Mar",
+//   "Apr",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+
+// var dateNow = new Date();
+
+// var year = dateNow.getFullYear();
+// var month = dateNow.getMonth();
+// var date = dateNow.getDate();
+// var day = dateNow.getDay();
+// var hours = dateNow.getHours();
+// var min = dateNow.getMinutes();
+// var sec = dateNow.getSeconds();
+// var millisec = dateNow.getMilliseconds();
+// var time = dateNow.getTime();
+
+// console.log(dateNow);
+// console.log(dateNow);
+// console.log(dateNow);
+// console.log(dateNow);
+// console.log(dateNow);
+// console.log(dateNow);
+// console.log(dateNow);
+// console.log(dateNow);
+// console.log(dateNow);
+// console.log(dateNow);
+// console.log(dateNow);
+// console.log(dateNow);
+// console.log(dateNow);
+// console.log(dateNow);
+
+// var currentDate = new Date();
+// var currentDateTime = currentDate.getTime();
+
+// var userBirthdayinput = prompt("Enter your Birth date");
+// var userBirthdayTime = new Date(userBirthdayinput);
+
+// var diffMiliSec = currentDateTime - userBirthdayTime;
+// var age = diffMiliSec / (1000 * 60 * 60 * 24 * 365);
+
+// console.log(age);
+// document.write(age);
+// alert(age);
+
+
