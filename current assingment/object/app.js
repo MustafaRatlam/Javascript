246979152c37ea7515d29d531135003240843610
Mustafa Ratlamwala <mustafaratlam110@gmail.com>
2022-01-02 20:04:26 +0500

    object practice

---
+++ current assingment/object/app.js
@@ -0,0 +1,36 @@
+var student1 = {
+  name: "Mustafa",
+  fathername: "Fakhruddin",
+  age: 16,
+  contact: "03370366926",
+};
+var student1 = {
+  name: "Mustafa",
+  fathername: "Fakhruddin",
+  age: 16,
+  contact: "03370366926",
+};
+var student2 = {
+  name: "Mustafa",
+  fathername: "Fakhruddin",
+  age: 16,
+  contact: "03370366926",
+};
+var student3 = {
+  name: "Mustafa",
+  fathername: "Fakhruddin",
+  age: 16,
+  contact: "03370366926",
+};
+var student4 = {
+  name: "Mustafa",
+  fathername: "Fakhruddin",
+  age: 16,
+  contact: "03370366926",
+};
+var student5 = {
+  name: "Mustafa",
+  fathername: "Fakhruddin",
+  age: 16,
+  contact: "03370366926",
+};
