8845e0b1f1286b30aed563d7fca113a3d6a492ab
Mustafa Ratlamwala <mustafaratlam110@gmail.com>
2022-01-03 19:24:13 +0500

    Object project

--- current assingment/object/app.js
+++ current assingment/object/app.js
@@ -3,42 +3,42 @@ var student1 = {
   fatherName: "Fakhruddin",
   age: 16,
   contact: "03370366926",
-  isClearInAllSubjects:true,
+  isClearInAllSubjects: true,
 };
 var student2 = {
   name: "Mustafa",
   fatherName: "Fakhruddin",
   age: 16,
   contact: "03370366926",
-  isClearInAllSubjects:true,
+  isClearInAllSubjects: true,
 };
 var student3 = {
   name: "Mustafa",
   fatherName: "Fakhruddin",
   age: 16,
   contact: "03370366926",
-  isClearInAllSubjects:true,
+  isClearInAllSubjects: true,
 };
 var student4 = {
   name: "Mustafa",
   fatherName: "Fakhruddin",
   age: 16,
   contact: "03370366926",
-  isClearInAllSubjects:true,
+  isClearInAllSubjects: true,
 };
 var student5 = {
   name: "Mustafa",
   fatherName: "Fakhruddin",
   age: 16,
   contact: "03370366926",
-  isClearInAllSubjects:true,
+  isClearInAllSubjects: true,
 };
-var student6= {
+var student6 = {
   name: "Mustafa",
   fatherName: "Fakhruddin",
   age: 16,
   contact: "03370366926",
-  isClearInAllSubjects:true,
+  isClearInAllSubjects: true,
 };
 
 //edit
@@ -53,7 +53,7 @@ student5.motherName = "Zainab";
 student6.motherName = "Zainab";
 
 //saving student data in an array
-var studentList = [student1, student2, student3, student4, student5,student6];
+var studentList = [student1, student2, student3, student4, student5, student6];
 
 //for roll number
 for (var i = 0; i < studentList.length; i++) {
@@ -63,16 +63,45 @@ for (var i = 0; i < studentList.length; i++) {
 //for data showing
 var studentData = document.getElementById("studentData");
 
-for(var i=0;i<studentList.length; i++){
-  var obj=studentList[i]
-  console.log(obj)
+for (var i = 0; i < studentList.length; i++) {
+  var obj = studentList[i];
+  console.log(obj);
 
   studentData.innerHTML +=
-    "<tr><td>"+ obj.rollNumber + 
-    "</td><td>" + obj.name+ 
-    "</td><td>" + obj.fatherName+ 
-    "</td><td>" + obj.motherName+ 
-    "</td><td>" +obj.contact+
-    "</td><td>" +obj.isClearInAllSubjects+
-    "</td></tr>"
+    "<tr><td>" +
+    obj.rollNumber +
+    "</td><td>" +
+    obj.name +
+    "</td><td>" +
+    obj.fatherName +
+    "</td><td>" +
+    obj.motherName +
+    "</td><td>" +
+    obj.contact +
+    "</td><td>" +
+    obj.isClearInAllSubjects +
+    "</td></tr>";
+}
+
+function searchStd() {
+  var inpVal = document.getElementById("inpVal");
+  var showRollNumber = document.getElementById("showRollNumber");
+  var showName = document.getElementById("showName");
+  var showFatherName = document.getElementById("showFatherName");
+  var showMotherName = document.getElementById("showMotherName");
+  var showContact = document.getElementById("showContact");
+  var showResult = document.getElementById("showResult");
+
+  for (var i = 0; i < studentList.length; i++) {
+    var std = studentList[i];
+
+    if (inpVal.value == std.rollNumber) {
+      showRollNumber.innerHTML = std.rollNumber;
+      showName.innerHTML = std.name;
+      showFatherName.innerHTML = std.fatherName;
+      showMotherName.innerHTML = std.motherName;
+      showContact.innerHTML = std.contact;
+      showResult.innerHTML = std.isClearInAllSubjects;
+    }
+  }
 }
