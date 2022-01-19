2194e5fce19e5bda49ffdaf47129150bf73d4cd4
Mustafa Ratlamwala <mustafaratlam110@gmail.com>
2022-01-19 19:13:53 +0500

    signup

--- current assingment/Browser Control/app.js
+++ current assingment/Browser Control/app.js
@@ -1,14 +1,29 @@
-window.location.href; // URL
-window.location.hostname; //Host name
-window.location.pathname; //Define the current pathname
-window.location.assign(""); //assign new URL
-window.location.replace(""); //Replace URL
-window.location.reload(); // browser reload
-window.location.reload(false); // browser reload
-window.location.reload(true); // server reload
-history.back(); // move back screen
-history.forward(); // move forward screen
-history.go(1); // move 1 page forward
-history.go(-1); // move 1 page backward 
-history.go(-2); // move 2 page backward
-document.referrer //user reference
+// window.location.href; // URL
+// window.location.hostname; //Host name
+// window.location.pathname; //Define the current pathname
+// window.location.assign(""); //assign new URL
+// window.location.replace(""); //Replace URL
+// window.location.reload(); // browser reload
+// window.location.reload(false); // browser reload
+// window.location.reload(true); // server reload
+// history.back(); // move back screen
+// history.forward(); // move forward screen
+// history.go(1); // move 1 page forward
+// history.go(-1); // move 1 page backward 
+// history.go(-2); // move 2 page backward
+// document.referrer //user reference
+
+//SignUp Form
+
+var
+
+
+
+function checkValue(e){
+    e.preventDefault()
+
+
+
+
+
+}
