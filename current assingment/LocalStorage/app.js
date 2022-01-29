96228c61cc355a5a5c5af5b6ada77e988192649d
Mustafa Ratlamwala <mustafaratlam110@gmail.com>
2022-01-29 18:04:32 +0500

    code

---
+++ current assingment/LocalStorage/app.js
@@ -0,0 +1,36 @@
+var student1 = {
+  name: "Mustafa",
+  fatherName: "Fakhruddin",
+  age: 16,
+  contact: "03370366926",
+  isClearInAllSubjects: true,
+};
+var student2 = {
+  name: "Mustafa",
+  fatherName: "Fakhruddin",
+  age: 16,
+  contact: "03370366926",
+  isClearInAllSubjects: true,
+};var student3 = {
+  name: "Mustafa",
+  fatherName: "Fakhruddin",
+  age: 16,
+  contact: "03370366926",
+  isClearInAllSubjects: true,
+};
+
+
+
+function addData(){
+    localStorage.setItem()
+    
+}
+function getData() {
+  localStorage.setItem();
+}
+function clearData() {
+  localStorage.setItem();
+}
+function clearAll() {
+  localStorage.setItem();
+}
