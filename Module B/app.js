13e1aae34695999572815370d1359d60cbc0cc3e
MustafaRatlam <mustafaratlam110@gmail.com>
2022-03-29 17:43:30 +0500

    Assingment (extracting object and keys done)

---
+++ Module B/app.js
@@ -0,0 +1,14 @@
+let mustafa = {
+  age: 20,
+  fatherName: "fakhruddin",
+  height: 5.6,
+};
+
+// extracting keys and values separately
+
+let keys = Object.keys(mustafa);
+
+console.log(keys);
+
+let values = Object.values(mustafa);
+console.log(values);
