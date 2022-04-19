44dffe31e8ed44d00acc0c08d57bfef7a35aca64
MustafaRatlam <mustafaratlam110@gmail.com>
2022-04-19 13:36:16 +0500

    Todo List Assingment Code

--- MERN_STACK/react/firstapp/src/App.js
+++ MERN_STACK/react/firstapp/src/App.js
@@ -1,22 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import { Button, Alert, Card ,Form} from "react-bootstrap/";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [arr, setArr]= useState(["a","b","c","d","e"])
-
+  const [arr, setArr]= useState(["1","2","3","4","5"])
+  const [text, setText] = useState("")
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello</p>
-        {/* <input
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Enter Text"
-        />
+        
+        <Form>
+          <Form.Group>
+            <Form.Control type="text" placeholder="Enter your Memo" ></Form.Control>
+          </Form.Group>
+        </Form>
+        
       
-        <p>{text}</p> */}
+        <p>{text}</p>
         {arr.map((e, i)=>{return <h1>{e}</h1>})}
       <p></p>
       </header>
