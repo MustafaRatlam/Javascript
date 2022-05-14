0fd4ef495e437e1cb28cf21e0dc2d564680d1860
MustafaRatlam <mustafaratlam110@gmail.com>
2022-05-15 04:32:45 +0500

    card done

--- MERN_STACK/react/firstapp/src/App.js
+++ MERN_STACK/react/firstapp/src/App.js
@@ -2,32 +2,98 @@ import "./App.css";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  const [text, setText] = useState("");
-  const [textArr, setTextArr] = useState([]);
+import Card from "./card";
 
-  const addTodo = () => {
-    setTextArr([...textArr, text]);
-    setText("");
+function App() {
+  // const [text, setText] = useState("");
+  // const [textArr, setTextArr] = useState([]);
+  const [arr, setArray] = useState([
+    {
+      title: "ABC",
+      price: 987,
+      category: " fafiahfahf",
+      size: "xl",
+      id: 2,
+      imageURl: "https://picsum.photos/200",
+    },
+    {
+      title: "ABC",
+      price: 987,
+      category: " fafiahfahf",
+      size: "xl",
+      id: 2,
+      imageURl: "https://picsum.photos/200",
+    },
+    {
+      title: "ABC",
+      price: 987,
+      category: " fafiahfahf",
+      size: "xl",
+      id: 2,
+      imageURl: "https://picsum.photos/200",
+    },
+    {
+      title: "ABC",
+      price: 987,
+      category: " fafiahfahf",
+      size: "xl",
+      id: 2,
+      imageURl: "https://picsum.photos/200",
+    },
+    {
+      title: "ABC",
+      price: 987,
+      category: " fafiahfahf",
+      size: "xl",
+      id: 2,
+      imageURl: "https://picsum.photos/200",
+    },
+    {
+      title: "ABC",
+      price: 987,
+      category: " fafiahfahf",
+      size: "xl",
+      id: 2,
+      imageURl: "https://picsum.photos/200",
+    },
+    {
+      title: "ABC",
+      price: 987,
+      category: " fafiahfahf",
+      size: "xl",
+      id: 2,
+      imageURl: "https://picsum.photos/200",
+    },
+    {
+      title: "ABC",
+      price: 987,
+      category: " fafiahfahf",
+      size: "xl",
+      id: 2,
+      imageURl: "https://picsum.photos/200",
+    },
+  ]);
+  // const addTodo = () => {
+  //   setTextArr([...textArr, text]);
+  //   setText("");
+  // };
+  let adToCart = () => {
+    console.log("Add To Cart");
   };
   return (
     <div className="App">
       <header className="App-header">
-        <input
-          type="text"
-          value={text}
-          className="form-control-lg mb-3"
-          placeholder="Enter text"
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button className="btn btn-primary" onClick={() => addTodo()}>
-          {""}
-          Add
-        </button>
-
-        {textArr.map((e, i) => {
-          return <h1 key={i}>{e}</h1>;
-        })}
+        {arr.map((e) => (
+          <Card
+            title={e.title}
+            price={e.price}
+            category={e.category}
+            size={e.size}
+            id={e.id}
+            click={() => adToCart()}
+            image={e.imageURl}
+          />
+        ))}
       </header>
     </div>
   );
