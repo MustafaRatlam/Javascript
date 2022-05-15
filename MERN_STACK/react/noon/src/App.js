11c5aa0d6fb555be6dcbb3c579a260260883ffce
MustafaRatlam <mustafaratlam110@gmail.com>
2022-05-16 00:49:14 +0500

    Landing page

---
+++ MERN_STACK/react/noon/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ReactDOM } from "react";
+import { Button } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MUI</h1>
+        <Button>MUI</Button>
+        </header>
+    </div>
+  );
+}
+
+export default App;
